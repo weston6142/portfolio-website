ff78d5449476d17cbc5332d0616e6d20fe166b90
weston6142 <wbushyeager6142@gmail.com>
2023-09-12 08:28:59 -0400

    Camera changes needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Experience from "./components/Experience";
+
+// stylize App div
+const AppDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppDiv>
+      <Experience />
+    </AppDiv>
   );
 }
 

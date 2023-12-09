c5a11af6557cf41b57150ec4634b1d787cd0d7ac
weston6142 <wbushyeager6142@gmail.com>
2023-12-09 16:10:05 -0500

    Update camera position and add useControls for MovingSpot position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,30 +4,81 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import { Vector3 } from "three";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useGLTF, SpotLight, useDepthBuffer } from "@react-three/drei";
+import { useControls } from "leva";
 
 // setup Experience component
+
 const Experience = (props) => {
+  const { x1, y1, z1 } = useControls({
+    x1: {
+      value: -3,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+    y1: {
+      value: 3.1,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+    z1: {
+      value: 1.7,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+  });
+
+  const { x2, y2, z2 } = useControls({
+    x2: {
+      value: 2.4,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+    y2: {
+      value: 3.2,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+    z2: {
+      value: 1.7,
+      min: -10,
+      max: 10,
+      step: 0.1,
+    },
+  });
+
   return (
     <Canvas
       shadows
       dpr={[1, 2]}
-      camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}
+      camera={{ position: [0, 2, 7], fov: 50, near: 1, far: 20 }}
     >
-      <color attach="background" args={["#202020"]} />
-      <fog attach="fog" args={["#202020", 5, 20]} />
-      <ambientLight intensity={0.015} />
-      <Scene />
+      {/* <color attach="background" args={["#202020"]} /> */}
+      <color attach="background" args={["black"]} />
+
+      {/* <fog attach="fog" args={["#202020", 5, 20]} /> */}
+      <fog attach="fog" args={["black", 5, 20]} />
+
+      <ambientLight intensity={0.095} />
+      <Scene x1={x1} y1={y1} z1={z1} x2={x2} y2={y2} z2={z2} />
+      <axesHelper args={[1]} />
     </Canvas>
   );
 };
 
-function Scene() {
+function Scene({ x1, y1, z1, x2, y2, z2 }) {
   // This is a super cheap depth buffer that only renders once (frames: 1 is optional!), which works well for static scenes
   // Spots can optionally use that for realism, learn about soft particles here: http://john-chapman-graphics.blogspot.com/2013/01/good-enough-volumetrics-for-spotlights.html
   const depthBuffer = useDepthBuffer({ frames: 1 });
   // const { nodes, materials } = useGLTF(
   //   "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/dragon/model.gltf"
   // );
+
+  // create use controls for MovingSpot position
 
   const { scene } = useGLTF(
     "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/react-logo/model.gltf"
@@ -37,12 +88,12 @@ function Scene() {
       <MovingSpot
         depthBuffer={depthBuffer}
         color="#000077"
-        position={[3, 3, 2]}
+        position={[x1, y1, z1]}
       />
       <MovingSpot
         depthBuffer={depthBuffer}
         color="#6A2ECD"
-        position={[1, 3, 0]}
+        position={[x2, y2, z2]}
       />
       {/* <mesh
         position={[0, -1.03, 0]}
@@ -52,10 +103,10 @@ function Scene() {
         material={materials["Default OBJ.001"]}
         dispose={null}
       /> */}
-      <primitive object={scene} />
-      <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
+      <primitive receiveShadow castShadow object={scene} />
+      <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[50, 50]} />
-        <meshPhongMaterial />
+        <meshStandardMaterial color={"#eeeeee"} />
       </mesh>
     </>
   );
@@ -84,7 +135,7 @@ function MovingSpot({ vec = new Vector3(), ...props }) {
       angle={0.35}
       attenuation={5}
       anglePower={4}
-      intensity={2}
+      intensity={10}
       {...props}
     />
   );

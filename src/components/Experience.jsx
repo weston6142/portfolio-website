79bfdca763e60f92863e24b553a443d517fc3a8f
weston6142 <wbushyeager6142@gmail.com>
2023-12-27 09:59:44 -0500

    Refactor NavBillboard component to ahndle clicks on sections

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -234,15 +234,38 @@ const NavBillboard = () => {
   const { camera } = useThree();
   const thresholdY = -1.5; // Define the threshold Y position
   const [showScreen, setShowScreen] = useState(false);
-  const previousY = useRef(camera.position.y);
-  const aboutSectionHeight = -1.5;
-  const experienceSectionHeight = -3.5;
-  const projectsSectionHeight = -6.5;
   const [aboutSectionText, setAboutSectionText] = useState("--- About");
+
   const [experienceSectionText, setExperienceSectionText] =
     useState("--- Experience");
   const [projectsSectionText, setProjectsSectionText] =
     useState("--- Projects");
+
+  const previousY = useRef(camera.position.y);
+  const aboutSectionHeight = -1.5;
+  const experienceSectionHeight = -3.5;
+  const projectsSectionHeight = -6.5;
+
+  const handleAboutClick = (event) => {
+    event.stopPropagation();
+    camera.position.y = aboutSectionHeight - 1;
+  };
+  const handleExperienceClick = (event) => {
+    event.stopPropagation();
+    camera.position.y = experienceSectionHeight - 3;
+  };
+
+  const handleProjectsClick = (event) => {
+    event.stopPropagation();
+    camera.position.y = projectsSectionHeight - 3;
+  };
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "auto";
+  };
 
   const [spring, setSpring] = useSpring(() => ({
     rotation: [0, 0, 0],
@@ -333,33 +356,59 @@ const NavBillboard = () => {
           <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
           {"I build software that solves problems."}
         </AnimatedText3D>
-        <AnimatedText3D
-          scale={0.006}
-          font={"/Roboto_Regular.json"}
+        <group
           position={[-0.2, 0.0, 0]}
-          {...spring}
+          onClick={handleAboutClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
         >
-          <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
-          {aboutSectionText}
-        </AnimatedText3D>
-        <AnimatedText3D
-          scale={0.006}
-          font={"/Roboto_Regular.json"}
+          <AnimatedText3D
+            scale={0.006}
+            font={"/Roboto_Regular.json"}
+            {...spring}
+          >
+            <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
+            {aboutSectionText}
+          </AnimatedText3D>
+        </group>
+        <group
           position={[-0.2, -0.01, 0]}
-          {...spring}
+          onClick={handleExperienceClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
         >
-          <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
-          {experienceSectionText}
-        </AnimatedText3D>
-        <AnimatedText3D
-          scale={0.006}
-          font={"/Roboto_Regular.json"}
+          <mesh scale={0.1} position={[0.025, 0.003, 0]}>
+            <planeGeometry args={[0.5, 0.1]} />
+            <meshBasicMaterial visible={false} />
+          </mesh>
+          <AnimatedText3D
+            scale={0.006}
+            font={"/Roboto_Regular.json"}
+            {...spring}
+          >
+            <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
+            {experienceSectionText}
+          </AnimatedText3D>
+        </group>
+        <group
           position={[-0.2, -0.02, 0]}
-          {...spring}
+          onClick={handleProjectsClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
         >
-          <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
-          {projectsSectionText}
-        </AnimatedText3D>
+          <mesh scale={0.1} position={[0.025, 0.003, 0]}>
+            <planeGeometry args={[0.5, 0.1]} />
+            <meshBasicMaterial visible={false} />
+          </mesh>
+          <AnimatedText3D
+            scale={0.006}
+            font={"/Roboto_Regular.json"}
+            {...spring}
+          >
+            <meshBasicMaterial color={theme.colors.white} toneMapped={false} />
+            {projectsSectionText}
+          </AnimatedText3D>
+        </group>
 
         <AnimatedLinkedInIcon
           position={[-0.2, -0.1, 0]}
